87ebf4a8a290e10609ffc8b082093171da107701
Anibal <anibal.siguenza1@gmail.com>
2017-05-24 16:40:31 +0200

    First commit

---
+++ student/histogram_par.cpp
@@ -0,0 +1,12 @@
+#include "histogram.h"
+#include "names.h"
+
+#include <future>
+#include <thread>
+#include <vector>
+#include <algorithm>
+
+void get_histogram(const std::vector<word_t>& words, histogram_t& histogram, int num_threads)
+{
+    // put your code here
+}
