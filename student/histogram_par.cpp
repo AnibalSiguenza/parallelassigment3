9ee47049af4f2d4cefdfd48827d33a525e3af37c
Anibal <anibal.siguenza1@gmail.com>
2017-06-02 12:40:47 +0200

    final version

--- student/histogram_par.cpp
+++ student/histogram_par.cpp
@@ -8,11 +8,7 @@
 #include  <iostream> 
 #include <iterator>
 
-histogram_t count_words(const std::vector<word_t> & words, int vec_init, int vec_end){
-    histogram_t histogram;
-    for(int j=0; j<NNAMES; j++){
-        histogram[j]=0;
-    }
+histogram_t count_words(const std::vector<word_t> & words, int vec_init, int vec_end, histogram_t histogram){
     std::for_each(std::next(words.begin(), vec_init), std::next(words.begin(), vec_end), [&histogram](const word_t& word)
             {
             int res = getNameIndex(word.data());
@@ -21,27 +17,27 @@ histogram_t count_words(const std::vector<word_t> & words, int vec_init, int vec
     return histogram;
 }
 
-void get_histogram(const std::vector<word_t> & words, histogram_t & histogram,const int num_threads){
+void get_histogram(const std::vector<word_t> & words, histogram_t & histogram, const int num_threads){
     std::vector<std::future<histogram_t>> futures;
-    //histogram_t * histograms_threads=new histogram_t[num_threads];
 
     int num_words=words.size(); 
     int nWordsPThreads=num_words/num_threads;
     int resWords=num_words%nWordsPThreads; 
 
+    for(int j=0; j<NNAMES; j++){
+        histogram[j]=0;
+    }
+
     if(resWords==0){
         for(int i=0;i<num_threads;i++){
-            futures.push_back(std::async(std::launch::async, count_words, std::ref(words), i*nWordsPThreads, (i+1)*nWordsPThreads-1));
+            futures.push_back(std::async(std::launch::async, count_words, std::ref(words), i*nWordsPThreads, (i+1)*nWordsPThreads-1, histogram));
         }
     }else{
         for(int i=1;i<num_threads;i++){
-           futures.push_back(std::async(std::launch::async, count_words, std::ref(words), (i-1)*nWordsPThreads, i*nWordsPThreads-1));
+           futures.push_back(std::async(std::launch::async, count_words, std::ref(words), (i-1)*nWordsPThreads, i*nWordsPThreads-1, histogram));
         }
-        futures.push_back (std::async(std::launch::async, count_words, std::ref(words), (num_threads-1)*nWordsPThreads, (num_threads)*nWordsPThreads+resWords-1));
-    }
-
-    for(int j=0; j<NNAMES; j++){
-        histogram[j]=0;
+        futures.push_back (std::async(std::launch::async, count_words, std::ref(words), (num_threads-1)*nWordsPThreads,\
+                    (num_threads)*nWordsPThreads+resWords-1, histogram));
     }
 
     for(int i=0;i<num_threads;i++){
@@ -50,5 +46,4 @@ void get_histogram(const std::vector<word_t> & words, histogram_t & histogram,co
             histogram[j]+=temp_histo[j];
         }
     }
-    //delete [] histograms_threads;
 }
